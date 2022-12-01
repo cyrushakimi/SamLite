349758220f33134c922b0f96a4792bd855c78e83
cyrushakimi <cyrush@vt.edu>
2022-12-01 14:31:02 -0500

    security issuesgit add main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from OpenSSL import SSL
 print("hello sam")
 payload = {'limit': "1", "api_key": "0t24jH6Rn6s5XKRFQFDolcFRRZsk9D1YQlZoh5xG", "postedFrom": "01/01/2018", "postedTo": "05/10/2018"}
 
-x = requests.get("https://api.sam.gov/prod/opportunities/v2/search?",params=payload)
+x = requests.get("https://api.sam.gov/prod/opportunities/v2/search?",params=payload, verify=False)
 
 print(x.text)
